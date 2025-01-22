44b5fd0858a5b5dd882cdf2b2b5b42b3dc1e252f
chocozzangzzang <jangjh_05@naver.com>
2025-01-22 15:17:20 +0900

    vuejs 문법 - v-for 및  modal의 사용(각 요소 별 modal)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <template>
+
+    <!-- 모달창 : 기존에 등록해두고 호출하는 형태 -->
+    <!-- v-if 조건식 -->
+    <div class="black-bg" v-if="modalIsOpen">
+      <div class="white-bg">
+        <h3>상세 페이지</h3>
+        <h4>{{ modalTitle }}</h4>
+        <p>{{ modalContent }}</p>
+        <button @click="modalClose">닫기</button>
+      </div>
+    </div>
+
   <div class="menu">
     <!-- 반복적인 요소의 축약 v-for
       v-for="변수명 in array" :key="변수명"
@@ -15,6 +27,7 @@
     <a>About</a>
     -->
   </div>
+
   <!-- {{ dataname }} 으로 값을 받아옴 -->
   <!-- 데이터바인딩 하는 이유
       1. 하드코딩은 값 변경이 어려움
@@ -27,12 +40,14 @@
       ++ HTML 속성도 바인딩 가능 ++
       ++ :속성 = "바인딩 변수명" ++
   -->
-  
-  <div v-for="(product, idx) in products" :key="idx">
-    <h4>{{ product.name }}</h4>  
-    <p>{{ product.price }} 만원</p>
-    <button @click="product.reportTimes+=1">허위매물신고</button> <span>신고 횟수 : {{ product.reportTimes }}</span>
+  <div class="room-container">
+    <div class="room" v-for="(oneroom, idx) in onerooms" :key="idx">
+      <img :src="oneroom.image" class="room-img">
+      <h4 @click="modalOpen(idx)">{{ oneroom.title }}</h4>  
+      <p>{{ oneroom.price }} 원</p>
+    </div>
   </div>
+  
 
   <!-- 
     이벤트 핸들러 : HTML 클릭 시 코드를 실행함
@@ -43,19 +58,20 @@
     mouseover : 마우스를 갖다대는 경우 
   -->
 
-<!--   
-  <div>
-    <h4>{{ homes[0] }}</h4>
+  <!-- <div>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="modalOpen">{{ homes[0] }}</h4>
     <p>50 만원</p>
-
     <button @click="reports[0]+=1">허위 매물 신고</button> <span>신고 수 : {{ reports[0] }}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4>{{ homes[1] }}</h4>
     <p>60 만원</p>
     <button @click="reports[1]+=1">허위 매물 신고</button> <span>신고 수 : {{ reports[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img">
     <h4>{{ homes[2] }}</h4>
     <p>70 만원</p>
     <button @click="reports[2]+=1">허위 매물 신고</button> <span>신고 수 : {{ reports[2] }}</span>
@@ -64,6 +80,8 @@
 </template>
 
 <script>
+
+import roomdata from "./data/rooms.js";
 
 // DataBinding //
 export default {
@@ -74,15 +92,23 @@ export default {
     return {
       //price1 : 80,
       //price2 : 70,
+      onerooms : roomdata,
       menus : ['Home', 'Shop', 'About'],
       homes : ['역삼동 원룸', '천호동 원룸', '마포구 원룸'],
       reports : [0, 0, 0],
       products : [
-        {name : '역삼동 원룸', price : 80, reportTimes : 0},
-        {name : '천호동 원룸', price : 70, reportTimes : 0},
-        {name : '마포구 원룸', price : 95, reportTimes : 0},
+        {name : '역삼동 원룸', price : 80},
+        {name : '천호동 원룸', price : 70},
+        {name : '마포구 원룸', price : 95},
       ],
       reportTimes : 0,
+      /* 
+        1. UI의 현재 상태를 데이터로 저장해둠
+        2. 데이터에 따라 UI가 어떻게 보일지 작성
+      */  
+      modalIsOpen : false,
+      modalTitle : "",
+      modalContent : "",
     }
   },
   // Vue에서의 함수 -> methods : {}
@@ -90,7 +116,17 @@ export default {
   methods: {
     increase() {
       this.reportTimes += 1;
-    }
+    },
+    modalOpen(idx) {
+      this.modalIsOpen = true;
+      this.modalTitle = this.onerooms[idx].title;
+      this.modalContent = this.onerooms[idx].content;
+    },
+    modalClose() {
+      this.modalIsOpen = false;
+      this.modalTitle = "";
+      this.modalContent = "";
+    },
   },
   components: {
   }
@@ -114,5 +150,44 @@ export default {
 .menu a {
   color: white;
   padding: 10px;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+/* 모달 창 디자인 */
+
+body {
+  margin : 0
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding : 20px;
+}
+
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr; /* 두 개의 동일한 너비 컬럼 */
+  gap: 20px; /* 컬럼 간 간격 */
+}
+
+.room {
+  flex: 1;
+  padding: 20px;
+  text-align: center;
 }
 </style>
